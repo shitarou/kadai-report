63bca7185cc314313f8e244af07356e5b9dcf80d
be-yan <shly_417@yahoo.co.jp>
2021-05-12 17:51:39 +0900

    Add Employee index

--- src/controllers/employees/EmployeesIndexServlet.java
+++ src/controllers/employees/EmployeesIndexServlet.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import models.Employee;
 import utils.DBUtil;
 
+
 @WebServlet("/employees/index")
 public class EmployeesIndexServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -21,8 +22,9 @@ public class EmployeesIndexServlet extends HttpServlet {
 
     public EmployeesIndexServlet() {
         super();
-
     }
+
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
